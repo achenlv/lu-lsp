eee96847b0aa5d48392c516ee158a8ce009887aa
Agris Pudans <agris.pudans@outlook.com>
2025-02-19 13:36:35 +0200

    taraaaam

--- pd_koks/LSP_PD3_agris_pudans.c
+++ pd_koks/LSP_PD3_agris_pudans.c
@@ -12,7 +12,7 @@ PD3 -- Direktoriju koka apstaigāšana
 
 #define MAX_PATH 256
 
-void meklet(const char *filename, const char *basedir) {
+void meklet(const char *filename, const char *basedir, const char *initdir) {
   DIR *d;
   struct dirent *de;
   char path[MAX_PATH];
@@ -25,8 +25,8 @@ void meklet(const char *filename, const char *basedir) {
   }
 
   /* Lasām direktorijas saturu */
-  while ((de = readdir(d)) != NULL) {
-    /* Izlaižam . un .. */
+  while ( (de = readdir(d)) != NULL ) {
+    /* Izlaižam . un .. , lai izvairītos no ieciklošanās */
     if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) {
       continue;
     }
@@ -44,19 +44,78 @@ void meklet(const char *filename, const char *basedir) {
 
     /* Izvada, ja fails ir meklētais */
     if (strcmp(de->d_name, filename) == 0) {
-      if (strcmp(basedir, ".") == 0) {
-        printf("./%s\n", de->d_name);
-      } else {
-        /* Ja ceļš sākas ar basedir, noņemam to un pievienojam ./ */
-        if (strncmp(path, basedir, strlen(basedir)) == 0) {
-          printf(".%s\n", path + strlen(basedir));
-        }
+      const char *rel_path = path + strlen(initdir);
+      if (*rel_path == '/') rel_path++;
+      printf("%s\n", rel_path);
+    }
+      /* Izlaižam direktorijas, kas ir vājās saites */
+      if (S_ISLNK(statbuf.st_mode)) {
+        continue;
       }
+    /* Rekursīvi iet tālāk apakšdirektorijās */
+    if (lstat(path, &statbuf) == 0) {
+      /* Izlaižam direktorijas, kas ir vājās saites */
+      if (S_ISLNK(statbuf.st_mode)) {
+        continue;
+      }
+      /* Rekursīvi ejam pa īstajām direktorijām */
+      if (S_ISDIR(statbuf.st_mode)) {
+        meklet(filename, path, initial_dir);
+      }
+    }
+  }
+
+  closedir(d);
+}
+
+
+void meklet2(const char *filename, const char *basedir, const char *initial_dir) {
+  DIR *d;
+  struct dirent *de;
+  char path[MAX_PATH];
+  struct stat statbuf;
+
+  /* Atveram direktoriju */
+  d = opendir(basedir);
+  if (d == NULL) {
+    return;
+  }
+
+  /* Lasām direktorijas saturu */
+  while ((de = readdir(d)) != NULL) {
+    /* Veidojam pilno ceļu */
+    if (strlen(basedir) + strlen(de->d_name) + 2 > MAX_PATH) {
+      continue;
+    }
+    
+    strcpy(path, basedir);
+    if (path[strlen(basedir) - 1] != '/') {
+      strcat(path, "/");
+    }
+    strcat(path, de->d_name);
+
+    /* Pārbaudām faila tipu */
+    if (lstat(path, &statbuf) != 0) {
+      continue;
+    }
+
+    /* Izlaižam . un .. un vājās saites */
+    if (strcmp(de->d_name, ".") == 0 || 
+        strcmp(de->d_name, "..") == 0 ||
+        S_ISLNK(statbuf.st_mode)) {
+      continue;
+    }
+
+    /* Izvada, ja fails ir meklētais */
+    if (strcmp(de->d_name, filename) == 0) {
+      const char *rel_path = path + strlen(initial_dir);
+      if (*rel_path == '/') rel_path++;
+      printf("%s\n", rel_path);
     }
 
     /* Rekursīvi iet tālāk apakšdirektorijās */
-    if (lstat(path, &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
-      meklet(filename, path);
+    if (S_ISDIR(statbuf.st_mode)) {
+      meklet(filename, path, initial_dir);
     }
   }
 
@@ -71,15 +130,13 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Izmantošana: %s <fails> <direktorija>\n", argv[0]);
     return 0;
   }
-
-  /* Pārbaudām vai otrais arguments ir direktorija */
   if (stat(argv[2], &statbuf) != 0 || !S_ISDIR(statbuf.st_mode)) {
     fprintf(stderr, "Kļūda: %s nav direktorija!\n", argv[2]);
     return -1;
   }
 
   /* Sākam meklēšanu */
-   meklet(argv[1], argv[2]); 
+   meklet(argv[1], argv[2], argv[2]); 
 
    
   return 0;
